69b111ce0c528047d5123eb89012b8db7f5fa93f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 15:57:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string = "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string = string.upcase
+  elsif string == string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else 
     return "NO, NOT SINCE 1938!"
